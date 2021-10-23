e1605facbde70356d45fec3dab13ea8acf23cae6
Kotaro Yoshimoto <pythagora.yoshimoto@gmail.com>
2021-10-23 21:34:25 +0900

    Add bt action builder

---
+++ behavior_tree_action_builder/README.md
@@ -0,0 +1,3 @@
+# behavior_tree_action_builder ![ROS2-Foxy](https://github.com/OUXT-Polaris/behavior_tree_action_builder/workflows/ROS2-Foxy/badge.svg)
+
+utility functions for behavior tree action nodes.
---
+++ behavior_tree_action_builder/include/behavior_tree_action_builder/action_node.hpp
@@ -0,0 +1,39 @@
+// Copyright (c) 2020, OUXT-Polaris
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#ifndef BEHAVIOR_TREE_ACTION_BUILDER__ACTION_NODE_HPP_
+#define BEHAVIOR_TREE_ACTION_BUILDER__ACTION_NODE_HPP_
+
+#include <behaviortree_cpp_v3/action_node.h>
+#include <behaviortree_cpp_v3/bt_factory.h>
+#include <string>
+
+namespace behavior_tree_action_builder
+{
+class ActionNode : public BT::SyncActionNode
+{
+public:
+  ActionNode(
+    const std::string & name,
+    const BT::NodeConfiguration & config)
+  :  BT::SyncActionNode(name, config)
+  {
+    setRegistrationID(name);
+  }
+
+protected:
+  std::string name;
+};
+}  // namespace behavior_tree_action_builder
+
+#endif  // BEHAVIOR_TREE_ACTION_BUILDER__ACTION_NODE_HPP_
