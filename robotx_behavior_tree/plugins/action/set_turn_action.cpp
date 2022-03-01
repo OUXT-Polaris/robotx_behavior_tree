4212c407ee335800f236449090a0e91be27806a7
kentaokamoto <kenntabsk@gmail.com>
2022-03-01 23:32:29 +0900

    fix error wt*

--- robotx_behavior_tree/plugins/action/set_turn_action.cpp
+++ robotx_behavior_tree/plugins/action/set_turn_action.cpp
@@ -25,18 +25,18 @@ namespace robotx_behavior_tree
 class SetTurnAction : public ActionROS2Node
 {
 public:
-  WaitAction(const std::string & name, const BT::NodeConfiguration & config)
+  SetTurnAction(const std::string & name, const BT::NodeConfiguration & config)
   : ActionROS2Node(name, config)
   {
   }
 
   static BT::PortsList providedPorts()
   {
-    return {OutputPort<double>("goal_x1"),     OutputPort<double>("goal_y1"),
-            OutputPort<double>("goal_x2"),     OutputPort<double>("goal_y2"),
-            OutputPort<double>("goal_x3"),     OutputPort<double>("goal_y3"),
-            OutputPort<double>("goal_x4"),     OutputPort<double>("goal_y4"),
-            OutputPort<double>("goal_x5"),     OutputPort<double>("goal_y5"),
+    return {BT::OutputPort<double>("goal_x1"), BT::OutputPort<double>("goal_y1"),
+            BT::OutputPort<double>("goal_x2"), BT::OutputPort<double>("goal_y2"),
+            BT::OutputPort<double>("goal_x3"), BT::OutputPort<double>("goal_y3"),
+            BT::OutputPort<double>("goal_x4"), BT::OutputPort<double>("goal_y4"),
+            BT::OutputPort<double>("goal_x5"), BT::OutputPort<double>("goal_y5"),
             BT::InputPort<double>("radius"),   BT::InputPort<int>("dir"),
             BT::InputPort<double>("pose_x"),   BT::InputPort<int>("pose_y"),
             BT::InputPort<double>("pose_v_x"), BT::InputPort<int>("pose_v_y")};
@@ -65,10 +65,14 @@ protected:
 
     if (!success) {
       thetap_ = std::atan2(pose_v_y_ - pose_y_, pose_v_x_ - pose_x_);
-
+      std::vector<double> goal(9);
       for (int i = 0; i < 5; i++) {
-            goal[i] = std::cos(-thetap_)*(pose_v_x_ - radius_*std::cos((36+72*i)*3.14/180) - std::sin(-thetap_)*(pose_v_y_ - radius_*dir_*std::sin((36+72*i)*3.14/180);
-            goal[i+1] = std::sin(-thetap_)*(pose_v_x_ - radius_*std::cos((36+72*i)*3.14/180) + std::cos(-thetap_)*(pose_v_y_ - radius_*dir_*std::sin((36+72*i)*3.14/180);
+        goal[i] =
+          std::cos(-thetap_) * (pose_v_x_ - radius_ * std::cos((36 + 72 * i) * 3.14 / 180)) -
+          std::sin(-thetap_) * (pose_v_y_ - radius_ * dir_ * std::sin((36 + 72 * i) * 3.14 / 180));
+        goal[i + 1] =
+          std::sin(-thetap_) * (pose_v_x_ - radius_ * std::cos((36 + 72 * i) * 3.14 / 180)) +
+          std::cos(-thetap_) * (pose_v_y_ - radius_ * dir_ * std::sin((36 + 72 * i) * 3.14 / 180));
       }
       setOutput<double>("goal_x1", goal[0]);
       setOutput<double>("goal_y1", goal[1]);
@@ -86,8 +90,6 @@ protected:
     }
     return BT::NodeStatus::SUCCESS;
   }
-
-  std::vector<double> goal(10);
   double radius_, pose_x_, pose_y_, pose_v_x_, pose_v_y_, thetap_;
   int dir_;
   bool isSet = false;
