f9d3086fc4ea39f1a8d1c066929af4563a7752ec
sarubito <nagaoaustineo@gmail.com>
2022-09-02 19:20:28 +0900

    update

--- robotx_behavior_tree/plugins/action/move_to_gate_action.cpp
+++ robotx_behavior_tree/plugins/action/move_to_gate_action.cpp
@@ -45,63 +45,66 @@ protected:
     std::vector<robotx_behavior_msgs::msg::TaskObject> green_buoy;
     std::vector<robotx_behavior_msgs::msg::TaskObject> red_buoy;
     const auto task_objects_array = getTaskObjects();
-    try {
-      for (size_t i = 0; i < task_objects_array->task_objects.size(); i++) {
-        if (task_objects_array->task_objects[i].object_kind == 1) {
-          green_buoy.push_back(task_objects_array->task_objects[i]);
-        } else if (task_objects_array->task_objects[i].object_kind == 2) {
-          red_buoy.push_back(task_objects_array->task_objects[i]);
-        } else {
-        }
-      }
-
-      buoy1_x = green_buoy[0].x;
-      buoy1_y = green_buoy[0].y;
-      buoy2_x = red_buoy[0].x;
-      buoy2_y = red_buoy[0].y;
-
-      goal_x = (buoy1_x + buoy2_x) / 2;
-      goal_y = (buoy1_y + buoy2_y) / 2;
-      goal_theta = 0.0;
-
-      if (goal_x && goal_y && goal_theta) {
-        goal.header.frame_id = "map";
-        goal.pose.position.x = goal_x;
-        goal.pose.position.y = goal_y;
-        goal.pose.position.z = 0.0;
-
-        goal.pose.orientation.w = 0.0;
-        goal.pose.orientation.x = 0.0;
-        goal.pose.orientation.y = 0.0;
-        goal.pose.orientation.z = 0.0;
-
-        goal_pub_->publish(goal);
-
-        RCLCPP_INFO(get_logger(), "Published! goal to through the gate");
+    if(task_objects_array){
+        RCLCPP_INFO(get_logger(), "AAAAAAAAAAAA");
         return BT::NodeStatus::RUNNING;
-      } else {
-        return BT::NodeStatus::FAILURE;
-      }
-
-      return BT::NodeStatus::FAILURE;
-
-    } catch (rclcpp::exceptions::InvalidTopicNameError & error) {
-      std::cerr << error.what() << '\n';
-      return BT::NodeStatus::FAILURE;
     }
+    // try {
+    //   for (size_t i = 0; i < task_objects_array->task_objects.size(); i++) {
+    //     if (task_objects_array->task_objects[i].object_kind == 1) {
+    //       green_buoy.push_back(task_objects_array->task_objects[i]);
+    //     } else if (task_objects_array->task_objects[i].object_kind == 2) {
+    //       red_buoy.push_back(task_objects_array->task_objects[i]);
+    //     } else {
+    //     }
+    //   }
+
+    //   buoy1_x = green_buoy[0].x;
+    //   buoy1_y = green_buoy[0].y;
+    //   buoy2_x = red_buoy[0].x;
+    //   buoy2_y = red_buoy[0].y;
+
+    //   goal_x = (buoy1_x + buoy2_x) / 2;
+    //   goal_y = (buoy1_y + buoy2_y) / 2;
+    //   goal_theta = 0.0;
+
+    //   if (goal_x && goal_y && goal_theta) {
+    //     goal.header.frame_id = "map";
+    //     goal.pose.position.x = goal_x;
+    //     goal.pose.position.y = goal_y;
+    //     goal.pose.position.z = 0.0;
+
+    //     goal.pose.orientation.w = 0.0;
+    //     goal.pose.orientation.x = 0.0;
+    //     goal.pose.orientation.y = 0.0;
+    //     goal.pose.orientation.z = 0.0;
+
+    //     goal_pub_->publish(goal);
+
+    //     RCLCPP_INFO(get_logger(), "Published! goal to through the gate");
+    //     return BT::NodeStatus::RUNNING;
+    //   }
+    //   return BT::NodeStatus::FAILURE;
+
+    // } catch (rclcpp::exceptions::InvalidTopicNameError & error) {
+    //   std::cerr << error.what() << '\n';
+    //   return BT::NodeStatus::FAILURE;
+    // }
   }
   BT::NodeStatus onRunning() override
   {
-    auto pose = getCurrentPose();
-    get_parameter("goal_tolerance", goal_tolerance_);
-    if (pose) {
-      distance = getDistance(pose.value(), goal.pose);
-    }
-    if (distance < goal_tolerance_) {
-      RCLCPP_INFO(get_logger(), "Throgh Goal : SUCCESS");
-      return BT::NodeStatus::SUCCESS;
-    }
-    return BT::NodeStatus::RUNNING;
+    // auto pose = getCurrentPose();
+    // get_parameter("goal_tolerance", goal_tolerance_);
+    // if (pose) {
+    //   distance = getDistance(pose.value(), goal.pose);
+    // }
+    // if (distance < goal_tolerance_) {
+    //   RCLCPP_INFO(get_logger(), "Throgh Goal : SUCCESS");
+    //   return BT::NodeStatus::SUCCESS;
+    // }
+    // return BT::NodeStatus::RUNNING;
+    RCLCPP_INFO(get_logger(), "BBBBBBBBBB");
+    return BT::NodeStatus::SUCCESS;
   }
 
   double getDistance(const geometry_msgs::msg::Pose pose1, const geometry_msgs::msg::Pose pose2)
